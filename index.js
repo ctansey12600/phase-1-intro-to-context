3ed2225ca6499a680b947343eeeab56f0f2e4b35
caseytansey <ctansey12600@outlook.com>
2021-09-08 22:07:36 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,114 +1,151 @@
-function createEmployeeRecord(array){
-    let obj = {}
-    obj.firstName = array[0]
-    obj.familyName = array[1]
-    obj.title = array[2]
-    obj.payPerHour = array[3]
-    obj.timeInEvents = []
-    obj.timeOutEvents = []
-    return obj
+//Returns
+// JavaScript Object with keys:
+// firstName
+// familyName
+// title
+// payPerHour
+// timeInEvents
+// timeOutEvents
+
+const { template } = require("babel-core")
+
+function createEmployeeRecord(arrayOfRecords){
+    const employeeRecords = {
+        firstName: `${arrayOfRecords[0]}`,
+        familyName: `${arrayOfRecords[1]}`,
+        title: `${arrayOfRecords[2]}`,
+        payPerHour: arrayOfRecords[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecords
 }
 
+//Converts each nested Array into an employee record using 
+//createEmployeeRecord and accumulates it to a new Array
 
-function createEmployeeRecords(array){
-    let emptyArray = array.map((value => {
-        return createEmployeeRecord(value)
-    }))
-    return emptyArray
+function createEmployeeRecords(arrayOfRecord){
+   const arrayOfRecords = []
+    arrayOfRecord.map(res => {
+        arrayOfRecords.push(createEmployeeRecord(res))
+    })
+    return arrayOfRecords
 }
 
+//Returns The employee record
 
-
-function createTimeInEvent(object, time){
-    let splitTime = time.split(" ")
-    let numHour = parseInt(splitTime[1])
-    object.timeInEvents = [ {
-        type : "TimeIn",
-        hour : numHour,
-        date : splitTime[0]
-    }]
-    return object
+function createTimeInEvent(recordObj, inDate){
+    const splitDate = inDate.split(" ")
+    const date = splitDate[0]
+    const time = splitDate[1]
+    const timeInObj = {
+         type: "TimeIn",
+         hour: parseInt(time),
+         date: `${date}`
+     }
+     recordObj.timeInEvents.push(timeInObj)
+     return recordObj
 }
 
-function createTimeOutEvent(object, time){
-    let splitTime = time.split(" ")
-    let numHour = parseInt(splitTime[1])
-    object.timeOutEvents = [ {
-        type : "TimeOut",
-        hour : numHour,
-        date : splitTime[0]
-    }]
-    return object
+function createTimeOutEvent(recordObj, outDate){
+    const splitDate = outDate.split(" ")
+    const date = splitDate[0]
+    const time = splitDate[1]
+    const timeOutObj = {
+         type: "TimeOut",
+         hour: parseInt(time),
+         date: `${date}`
+     }
+     recordObj.timeOutEvents.push(timeOutObj)
+     return recordObj
 }
 
+//Returns Hours worked, an Integer
 
-
-function hoursWorkedOnDate(object, time){
-    let timeIn = object.timeInEvents
-    let timeOut = object.timeOutEvents
-    let hTimeIn = timeIn[0].date
-    let hTimeOut = timeOut[0].date
-
-    let totalIn
-    let totalOut
-
-    if( hTimeIn === `${time}`){
-        let num = timeIn[0].hour 
-        let string = num.toString()
-        let arrayIn = [`${string}`]
-        let value = `${arrayIn[0]}`
-        //for statement gets ['900'] to array of [ "9", "0"
-        let arrayValue = []
-        for (let i = 0; i < value.length; i++) {
-            arrayValue.push(value.charAt(i))
+function hoursWorkedOnDate(recordObj, date){
+    const timeIn = recordObj.timeInEvents
+    const hourIn = []
+    timeIn.filter((obj) => {
+        if(obj.date === date){
+            hourIn.push(obj.hour)
         }
-        let arraySingleValue = []
-        arrayValue.map((res) => {
-            if (res > 0){
-            arraySingleValue.push(parseInt(res))}
-        })
-        totalIn = parseInt(arraySingleValue.join(""))
-    } 
-
-    if( hTimeOut === `${time}`){
-        let num = timeOut[0].hour 
-        let string = num.toString()
-        let arrayOut = [`${string}`]
-        let value = `${arrayOut[0]}`
-        //for statement gets ['900'] to array of [ "9", "0"
-        let arrayValue = []
-        for (let i = 0; i < value.length; i++) {
-            arrayValue.push(value.charAt(i))
-        }
-        if (arrayValue.length < 4){
-         let arraySingleValue = []
-         arrayValue.map((res) => {
-            if (res > 0){
-            arraySingleValue.push(parseInt(res))}
-        })}
-        else if (arrayValue.length = 4){
-            
-        }
-        totalOut = parseInt(arraySingleValue.join(""))
-    } 
+    })
     
-    if(totalIn < totalOut){
-        return totalOut - totalIn
-    }else if(totalOut < totalIn){
-        return totalIn - totalOut 
+    const timeOut = recordObj.timeOutEvents
+    const hourOut = []
+    timeOut.filter((obj) => {
+        if(obj.date === date){
+            hourOut.push(obj.hour)
+        }
+    })
+
+    const reducer = (previousValue, currentValue) => previousValue + currentValue
+
+    const totalHourIn = hourIn.reduce(reducer)
+    const totalHourOut = hourOut.reduce(reducer)
+
+    const absoluteTotal = Math.abs(totalHourIn - totalHourOut)
+
+    if(absoluteTotal < 1000){
+        const string = absoluteTotal.toString().split(0,1)
+        return parseInt(string)
+    }else if(absoluteTotal > 999){
+        const string = absoluteTotal.toString()
+        const totalhour = string.charAt(0) + string.charAt(1)
+        return parseInt(totalhour)
     }
 }
 
-function wagesEarnedOnDate(object, time){
-    let hoursWorked = hoursWorkedOnDate(object, time)
-    let payRate = object.payPerHour
+// Behavior
+// Using hoursWorkedOnDate, multiply the hours by the 
+// record's payRate to determine amount owed. Amount should be returned as a number.
+
+function wagesEarnedOnDate(recordObj, date){
+    const payRate = recordObj.payPerHour
+    const hoursWorked = hoursWorkedOnDate(recordObj, date)
     return payRate * hoursWorked
 }
 
+// Behavior
+// Using wagesEarnedOnDate, accumulate the value of all dates worked 
+// by the employee in the record used as context. Amount 
+// should be returned as a number. HINT: You will need to find the 
+// available dates somehow...
 
+function allWagesFor(recordObj){
+    const inArray = recordObj.timeInEvents
+    const totalArray = []
+    inArray.map(res => {
+        const inDate = res.date
+        totalArray.push(wagesEarnedOnDate(recordObj, inDate))
+    })
 
-function allWagesFor(object){
-    let time = object.timeInEvents[0].date
-    console.log(object.timeInEvents[0].)
-    console.log(wagesEarnedOnDate(object, time))
+    const reducer = (previousValue, currentValue) => previousValue + currentValue
+
+    const totalWages = totalArray.reduce(reducer)
+    
+    return totalWages
 }
+
+// Behavior
+// Using wagesEarnedOnDate, accumulate the value of all 
+// dates worked by the employee in the record used as 
+// context. Amount should be returned as a number.
+
+function calculatePayroll(recordArray){
+    const totalPayrollArray = []
+    recordArray.map(singleObj => {
+        const recordObj = singleObj
+        const  totalTimeIn = recordObj.timeInEvents
+         totalTimeIn.map(singleTimeIn => {
+             const inDate = singleTimeIn.date
+            totalPayrollArray.push(wagesEarnedOnDate(recordObj, inDate))
+         })
+     })
+    
+     const reducer = (previousValue, currentValue) => previousValue + currentValue
+
+     const totalPayout = totalPayrollArray.reduce(reducer)
+    
+     return totalPayout
+ }
